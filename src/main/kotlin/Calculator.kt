d8f9bde58e3b73e6a9b7ab51c6f1e8d02908227f
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-10-30 14:42:48 +0700

    Add Comment

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,5 +1,6 @@
+//Membuat class yang akan di tes
 class Calculator {
-
+    //membuat function dengan parameter yang mereturn value int
     fun add(first: Int, second: Int):Int{
         return first + second
     }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,14 +1,17 @@
 import org.junit.jupiter.api.Test
 
+//Membuat class untuk test Calculator dengan nama menyesuaikan yang akan ditest
 class CalculatorTest {
 
+    //membuat variable untuk test dan memanggil object Calculator(yang ditest)
     val calculator = Calculator()
 
+    //function untuk test pertama
     @Test
     fun testAddSuccess(){
         val result = calculator.add(10,10)
     }
-
+    //function untuk test kedua(dua kali)
     @Test
     fun testAddSuccess2(){
         val result = calculator.add(10,10)
